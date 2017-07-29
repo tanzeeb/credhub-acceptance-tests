732673583522d90444202e74cd1ca2c791683384
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-07-28 17:55:06 -0700

    wip
    Signed-off-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>
    Signed-off-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- bbr_integration_test/bbr_integration_test.go
+++ bbr_integration_test/bbr_integration_test.go
@@ -6,22 +6,35 @@ import (
 	"github.com/cloudfoundry-incubator/credhub-acceptance-tests/test_helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
+	"encoding/json"
+	"strings"
 )
+
+type findResults struct {
+	Credentials []findResult `json:"credentials"`
+}
+
+type findResult struct {
+	Name string `json:"name"`
+	VersionCreatedAt string `json:"version_created_at"`
+}
 
 var _ = Describe("Backup and Restore", func() {
 	var credentialName string
 	var bbrTestPath = "bbr_test"
 
 	BeforeEach(func() {
+		var session *Session
 		credentialName = fmt.Sprintf("%s/%s", bbrTestPath, test_helpers.GenerateUniqueCredentialName())
 
 		By("authenticating against credhub")
-		session := RunCommand("credhub", "api", "--server", config.ApiUrl)
+
+		session = RunCommand("credhub", "api", "--server", config.ApiUrl, "--skip-tls-validation")
 		Eventually(session).Should(Exit(0))
 
-		session = RunCommand("credhub", "login", "-u", config.ApiUsername, "-p", config.ApiPassword)
+		session = RunCommand("credhub", "login", "--skip-tls-validation", "-u", config.ApiUsername, "-p", config.ApiPassword)
 		Eventually(session).Should(Exit(0))
 
 		CleanupCredhub(bbrTestPath)
@@ -33,8 +46,10 @@ var _ = Describe("Backup and Restore", func() {
 	})
 
 	It("Successfully backs up and restores a Credhub release", func() {
+		var session *Session
+
 		By("adding a test credential")
-		session := RunCommand("credhub", "set", "--name", credentialName, "--type", "password", "-w", "originalsecret")
+		session = RunCommand("credhub", "set", "--name", credentialName, "--type", "password", "-w", "originalsecret")
 		Eventually(session).Should(Exit(0))
 
 		By("running bbr backup")
@@ -45,35 +60,51 @@ var _ = Describe("Backup and Restore", func() {
 		By("asserting that the backup archive exists and contains a pg dump file")
 		session = RunCommand("sh", "-c", fmt.Sprintf("tar -xvf ./%s*Z/bosh*credhub.tar", config.DirectorHost))
 		Eventually(session).Should(Exit(0))
-		Eventually(RunCommand("ls", "credhubdb_dump")).Should(Exit(0))
+		Eventually(session).Should(Exit(0))
 
 		By("editing the test credential")
 		session = RunCommand("credhub", "set", "--name", credentialName, "--type", "password", "-w", "updatedsecret")
 		Eventually(session).Should(Exit(0))
 
-		editSession := RunCommand("credhub", "get", "--name", credentialName)
-		Eventually(editSession).Should(Exit(0))
-		Eventually(editSession.Out).Should(gbytes.Say("value: updatedsecret"))
+		session = RunCommand("credhub", "get", "--name", credentialName)
+		Eventually(session).Should(Exit(0))
+		Eventually(session.Out).Should(Say("value: updatedsecret"))
 
 		By("running bbr restore")
-		session = RunCommand("sh", "-c",
-			fmt.Sprintf("bbr director --private-key-path %s --username %s --host %s restore --artifact-path ./%s*Z/",
-				config.Bosh.SshPrivateKeyPath, config.Bosh.SshUsername, config.Bosh.Host,config.DirectorHost))
+		restoreCommand := fmt.Sprintf("bbr director --private-key-path %s --username %s --host %s restore --artifact-path ./%s*Z/",
+			config.Bosh.SshPrivateKeyPath, config.Bosh.SshUsername, config.Bosh.Host, config.DirectorHost)
+		session = RunCommand("sh", "-c", restoreCommand)
 		Eventually(session).Should(Exit(0))
 
 		By("checking if the test credentials was restored")
-		getSession := RunCommand("credhub", "get", "--name", credentialName)
-		Eventually(getSession).Should(Exit(0))
-		Eventually(getSession.Out).Should(gbytes.Say("value: originalsecret"))
+		session = RunCommand("credhub", "get", "--name", credentialName)
+		Eventually(session).Should(Exit(0))
+		Eventually(session.Out).Should(Say("value: originalsecret"))
 	})
 })
 
 func CleanupCredhub(path string) {
+	var session *Session
+	var credentials findResults
+
 	By("Cleaning up credhub bbr test passwords")
-	RunCommand(
-		"sh", "-c",
-		fmt.Sprintf("credhub find -p /%s | tail -n +2 | cut -d\" \" -f1 | xargs -IN credhub delete --name N", path),
-	)
+	namePrefix := fmt.Sprintf("/%s", path)
+	session = RunCommand("credhub", "find", "-p", namePrefix, "--output-json")
+
+	errorMessage := string(session.Err.Contents())
+
+	if !strings.Contains(errorMessage, "No credentials exist which match the provided parameters.") {
+		Eventually(session).Should(Exit(0))
+		results := string(session.Out.Contents())
+
+		err := json.Unmarshal([]byte(results), &credentials)
+		Expect(err).To(BeNil())
+
+		for _, credential := range credentials.Credentials {
+			session = RunCommand("credhub", "delete", "-n", credential.Name)
+			Eventually(session).Should(Exit(0))
+		}
+	}
 }
 
 func CleanupArtifacts() {
--- run_bbr_tests.sh
+++ run_bbr_tests.sh
@@ -2,19 +2,38 @@
 
 set -eu
 
-cat <<EOF > test_config.json
+if [[ -n "${BOSH_DEPLOYMENT:-}" ]]; then
+    cat <<EOF > config.json
 {
-  "director_host":"${API_IP}",
-  "api_url": "https://${API_IP}:8844",
-  "api_username":"${USERNAME}",
-  "api_password":"${PASSWORD}",
+  "director_host":"${DIRECTOR_IP}:22",
+  "api_url": "https://${CREDHUB_IP}:8844",
+  "api_username":"${CREDHUB_USERNAME}",
+  "api_password":"${CREDHUB_PASSWORD}",
   "bosh": {
     "host":"${API_IP}:22",
+    "director_username":"${BOSH_DIRECTOR_USERNAME}",
+    "director_password":"${BOSH_DIRECTOR_PASSWORD}",
+    "deployment":"${BOSH_DEPLOYMENT}"
+  },
+  "credential_root":"${CREDENTIAL_ROOT}",
+  "uaa_ca":"${UAA_CA}"
+}
+EOF
+else
+    cat <<EOF > config.json
+{
+  "director_host":"${DIRECTOR_IP}",
+  "api_url": "https://${CREDHUB_IP}:8844",
+  "api_username":"${CREDHUB_USERNAME}",
+  "api_password":"${CREDHUB_PASSWORD}",
+  "bosh": {
+    "host":"${DIRECTOR_IP}:22",
     "bosh_ssh_username":"${BOSH_SSH_USERNAME}",
     "bosh_ssh_private_key_path":"${BOSH_SSH_PRIVATE_KEY_PATH}"
   },
   "uaa_ca":"${UAA_CA}"
 }
 EOF
+fi
 
-ginkgo -r -p bbr_integration_test
+ginkgo -r bbr_integration_test
